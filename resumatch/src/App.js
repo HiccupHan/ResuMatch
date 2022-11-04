632a049dd80b87af0d1653d29833dc0098c79feb
HiccupHan <89772977+HiccupHan@users.noreply.github.com>
2022-11-03 23:05:00 -0700

    test popup

--- resumatch/src/App.js
+++ resumatch/src/App.js
@@ -3,13 +3,35 @@ import React from 'react'
 import Header from './components/Header'
 import MatchResults from './components/MatchResults'
 import Resumes from './components/Resumes'
+import Button from './components/Button'
+
+export const getCurrentTabUId = (callback) => {
+  const queryInfo = { active: true, currentWindow: true };
+
+  chrome.tabs &&
+    chrome.tabs.query(queryInfo, (tabs) => {
+      callback(tabs[0].id);
+    });
+};
 
 function App() {
+
+  const sendMessage = () => {
+    getCurrentTabUId((id) => {
+      id &&
+        chrome.tabs.sendMessage(id, {
+          value: "openPopup",
+        });
+      window.close();
+    });
+  };
+
   return (
     <div className="App">
       <Header />
       <MatchResults numberOfStars={3}/>
       <Resumes />
+      <Button />
     </div>
   );
 }
