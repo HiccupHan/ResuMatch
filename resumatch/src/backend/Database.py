906dd1cd784c68163acc06c762290c70dbdfc7e2
jagrit06 <jagrit06@gmail.com>
2022-11-29 17:24:46 -0800

    Resume removal done

--- resumatch/src/backend/Database.py
+++ resumatch/src/backend/Database.py
@@ -32,14 +32,10 @@ def get_resume_names(db):
 def add_resume(db, resume_file_path):
     user_data_dict = get_data_json(db)
 
-    print(user_data_dict)
-
     resume_data = {"file_name" : os.path.basename(resume_file_path), 
                    "raw_text": ResumeParser.get_raw_text(resume_file_path)}
 
     user_data_dict["resumes"].append(resume_data)
-
-    print(user_data_dict)
 
     db.put(b'user_data_dict', json.dumps(user_data_dict).encode())
 
@@ -47,12 +43,11 @@ def remove_resume(db, resume_file_name):
     user_data_dict = get_data_json(db)
     resumes = user_data_dict["resumes"]
 
-    for i, r in resumes: 
+    for i, r in enumerate(resumes): 
         if r["file_name"] == resume_file_name: 
             resumes.pop(i)
 
     user_data_dict["resumes"] = resumes
-
 
     db.put(b'user_data_dict', json.dumps(user_data_dict).encode())
     
